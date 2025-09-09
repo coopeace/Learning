ef75784122ea0206a8e1579a4ee78a57ed147e60
coopeace <shishirdev5263@gmail.com>
2025-09-09 17:47:26 +0530

    .gitingnore

--- C++/calculator.cpp
+++ C++/calculator.cpp
@@ -1,54 +1,38 @@
-#include <cstdlib>
 #include<iostream>
 
 int main(){
-  double a,b,c;
-  int key,f=0;
-  std::cout << "Welcome to the calsmart--->\n";
+  double a=0,b;
+  int choice;
   while(1){
-    if(f==0){
-      std::cout << "Enter 1st Num:";
-      std::cin >> a;
-      f=1;
-    }
-    std::cout << "Enter 2nd Num:";
-    std::cin >> b;
-    std::cout << "<---Arthmetic Operations--->\n";
-    std::cout << "1.Additon-(1)\n2.Substraction(2)\n3.Multiplication-(3)\n4.Divide-(4)\n";
-    std::cout << "Your chhoice:";
-    std::cin >> key;
-    switch(key){
-      case 1:
-        c=a+b;
-        std::cout << a << "+" << b << "=" << c << '\n';//printout a+b=c
-        a=c;
-        break;
-      case 2:
-        c=a-b;
-        std::cout << a << "-" << b  << "=" << c << '\n';//printout a-b=c
-        a=c;
-        break;
-      case 3:
-        c=a*b;
-        std::cout << a << "*" << b << "="  << c << '\n';//printout a*b=c
-        a=c;
-        break;
-      case 4:
-        c=a/b;
-        std::cout << a << "/" << b << "="  << c << '\n';//printout a/b=c
-        c=a;
-        break;
-      default:
-        std::cout << "Invalid chhoice\n";
-        std::cout << "Try Again\n";
-    }
-    char ques;
-    std::cout << "Do you want to continue(y/n):";
-    std::cin >> ques;
-    if(ques == 'n' || ques =='N'){
-      return 0;
+  std::cout << "************************\n";
+  std::cout << "  Arthmetic Opertions\n";
+  std::cout << "************************\n";
+  std::cout << "1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Quit\n";
+  std::cout << "************************\n";
+  std::cout << "ANS:" << a << '\n';
+  std::cout << "Enter your choice(1-5):";
+  std::cin >> choice;
+  if(choice == 5){
+    return 0;
   }
-    std::cout << "1st Num:" << a <<'\n';
-}
-return 0;
+  std::cout << "Enter your number:";
+  std::cin >> b;
+  switch(choice){
+    case '+':
+      a+=b;
+      break;
+    case '-':
+      a-=b;
+      break;
+    case '*':
+      a*=b;
+      break;
+    case '/':
+      a/=b;
+      break;
+    default:
+      std::cout << "Invalid Choice!!!\n";
+  }
+  }
+  return 0;
 }
